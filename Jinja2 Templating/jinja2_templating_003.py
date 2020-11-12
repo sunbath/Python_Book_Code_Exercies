cb1cf470f7d307f2a0b2e65ffd2ccc7b568024c3
Eric Leung <eric@Erics-MacBook-Pro.local>
2020-11-12 10:23:24 +0800

    Jinja2 StrictUndefined

---
+++ Jinja2 Templating/jinja2_templating_003.py
@@ -0,0 +1,10 @@
+from jinja2 import Template, StrictUndefined
+
+template = "Device {{ name }} is a {{ type }} located in the {{ site }} datacenter."
+
+data = {
+    "name": "waw-rtr-core-01",
+    "site": "warsaw-01",
+}
+
+j2_template = Template(template, undefined=StrictUndefined)
